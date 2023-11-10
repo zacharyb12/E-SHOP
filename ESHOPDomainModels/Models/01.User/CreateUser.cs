d1bcf866d105163ece304d4ad22ea6b6b9de8ba0
zachary boualem <boualemzachary89@yahoo.fr>
2023-11-10 19:55:07 +0100

    new commit

---
+++ ESHOPDomainModels/Models/01.User/CreateUser.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ESHOPDomainModels.Models.User
+{
+    public class CreateUser
+    {
+        [StringLength(50)]
+        public string LastName { get; set; }
+
+        [StringLength(50)]
+        public string FirstName { get; set; }
+
+        [EmailAddress]
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+
+        public string Status { get; set; }
+
+        [StringLength(255)]
+        public string Address { get; set; }
+    }
+}
